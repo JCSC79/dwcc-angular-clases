96ada5ca055921e543751c571f37ed309babab87
JCSC79 <jcsandomingo@egmail.com>
2026-08-30 01:24:05 +0200

    Automático: Añadido T05E05-enlazado-eventos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = '02-TypeScript';
+  title = '16-enlazado-eventos';
+  cor: string = "blue";
 
-  // Declaramos unha propiedade de clase (variable) de tipo numérico inicializada a un valor
-  numero1: number = 5;
-  /* Declaramos unha propiedade de clase de tipo numérico non inicializada (engadimos nas opcións de compilador do tsconfig.json a seguinte clave
-    "strictPropertyInitialization": false,)
-  */
-  numero2: number;
+  mudarTitulo(): void {
+    this.title = 'O botón de mudar título foi premido';
+  }
+
+  mudarCor(): void {
+    if (this.cor == "blue") {
+      this.cor = "purple";
+    } else {
+      this.cor = "blue";
+    }
+  }
 }
