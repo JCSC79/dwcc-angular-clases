b43c1f96a3a43109fb0665e5f276649c6c62ff1d
JCSC79 <jcsandomingo@egmail.com>
2026-08-30 01:24:26 +0200

    Automático: Añadido T05E16-ngFor-v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = '02-TypeScript';
+  title = '08-ngFor-v2';
+  equipos: string[];
+  division: string;
 
-  // Declaramos unha propiedade de clase (variable) de tipo numérico inicializada a un valor
-  numero1: number = 5;
-  /* Declaramos unha propiedade de clase de tipo numérico non inicializada (engadimos nas opcións de compilador do tsconfig.json a seguinte clave
-    "strictPropertyInitialization": false,)
-  */
-  numero2: number;
+  constructor() {
+    this.equipos = ["Celta de Vigo", "Real Madrid", "Barcelona", "Valencia", "Athletic Club", "Atlético de Madrid", "Real Betis Balompié"];
+    this.division = "Primeira";
+  }
+
+  mudarDivision() {
+    if (this.division == "Primeira") {
+      this.division = "Segunda";
+      this.equipos = ["Almería","Racing de Ferrol","Deportivo A Coruña","Albacete"];
+    } else {
+      this.division = "Primeira";
+      this.equipos = ["Celta de Vigo", "Real Madrid", "Barcelona", "Valencia", "Athletic Club", "Atlético de Madrid", "Real Betis Balompié"];
+    }
+
+  }
 }
