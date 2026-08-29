59ab84eb55e52663f75fb91ee7624c50d726b404
JCSC79 <jcsandomingo@egmail.com>
2026-08-30 01:29:44 +0200

    Automático: Añadido T06E08-animacions-listaxe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,85 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Tarefa } from './tarefa';
+import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  animations: [
+    trigger('entradaSaida', [
+      transition(':enter', [
+        animate('500ms ease-out', keyframes([
+          style({
+            transform: 'scale(0.5)',
+            opacity: 0,
+            offset: 0
+          }),
+          style({
+            transform: 'scale(1.1)',
+            opacity: 0.7,
+            offset: 0.7
+          }),
+          style({
+            transform: 'scale(1)',
+            opacity: 1,
+            offset: 1
+          }),
+        ]))
+      ]),
+      transition(':leave', [
+        animate('300ms ease-in', style({
+          transform: 'translateX(100%)',
+          opacity: 0
+        }))
+      ])
+    ]),
+    trigger('realzada', [
+      state('pendente', style({
+        backgroundColor: 'white',
+        transform: 'scale(1)'
+      })),
+      state('completada', style({
+        backgroundColor: 'green',
+        transform: 'scale(1.05)'
+      })),
+      transition('pendente <=> completada', [
+        animate('300ms ease-in-out')
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = '02-TypeScript';
+  title = 'Listaxe Animada';
+  formulario!: FormGroup;
+  tarefas: Tarefa[] = [];
 
-  // Declaramos unha propiedade de clase (variable) de tipo numérico inicializada a un valor
-  numero1: number = 5;
-  /* Declaramos unha propiedade de clase de tipo numérico non inicializada (engadimos nas opcións de compilador do tsconfig.json a seguinte clave
-    "strictPropertyInitialization": false,)
-  */
-  numero2: number;
+  constructor(private elaborador: FormBuilder) {
+    this.formulario = elaborador.group({
+      texto: []
+    });
+  }
+
+  engadir(): void {
+    if (this.formulario.get('texto')?.value) {
+      this.tarefas.push({
+        id: Date.now(),
+        texto: this.formulario.get('texto')?.value,
+        completada: false
+      });
+      this.formulario.reset();
+    }
+  }
+
+  completar(tarefa: Tarefa): void {
+    tarefa.completada = true;
+  }
+
+  eliminar(tarefa: Tarefa): void {
+    this.tarefas = this.tarefas.filter(dato => dato.id !== tarefa.id);
+  }
 }
