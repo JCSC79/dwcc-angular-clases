8d79d4b732095a40e61e8463479059d29bf5acd1
JCSC79 <jcsandomingo@egmail.com>
2026-08-30 01:30:03 +0200

    Automático: Añadido T06E18-formularios-reactivos-bulma

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,143 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = '02-TypeScript';
+  title = 'Formularios Reactivos con Validación';
+  formulario: FormGroup; // Definimos o FormGroup que irá asociado ao noso formulario HTML
 
-  // Declaramos unha propiedade de clase (variable) de tipo numérico inicializada a un valor
-  numero1: number = 5;
-  /* Declaramos unha propiedade de clase de tipo numérico non inicializada (engadimos nas opcións de compilador do tsconfig.json a seguinte clave
-    "strictPropertyInitialization": false,)
-  */
-  numero2: number;
+  // Declaramos unha propiedade privada de tipo FormBuilder que nos permitirá crear de xeito simple o formulario reactivo
+  constructor(private elaborador: FormBuilder) {
+    this.formulario = this.elaborador.group({
+      numParte: ['', [Validators.required]],
+      matricula: ['', [Validators.required]],
+      gravidade: ['', [Validators.required]],
+      descricion: ['', [Validators.required, Validators.maxLength(300)]],
+      data: [new Date().toISOString().split('T')[0], [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      telefono: ['', [Validators.required]],
+      numVitimas: [1, [Validators.required]]
+    });
+  }
+
+  // Comportamento do botón REXISTRAR
+  rexistrar(): void {
+    // Comprobamos se o formulario é válido
+    if (this.formulario.valid) {
+      console.table(this.formulario.value); // Imprimimos por consola o obxecto que contén os datos do formulario reactivo
+    } else {
+      this.formulario.markAllAsTouched();
+    }
+  }
+
+  // Comportamento do botón CANCELAR
+  cancelar(): void {
+    this.formulario.reset(); // Limpamos todos os campos do formulario reactivo (e do obxecto asociado)
+  }
+
+  // GETTERS para os controis de formulario (para facilitar o traballo dende HTML)
+  get campoNumParte() {
+    return this.formulario.get('numParte');
+  }
+
+  get campoMatricula() {
+    return this.formulario.get('matricula');
+  }
+
+  get campoGravidade() {
+    return this.formulario.get('gravidade');
+  }
+
+  get campoDescricion() {
+    return this.formulario.get('descricion');
+  }
+
+  get campoData() {
+    return this.formulario.get('data');
+  }
+
+  get campoEmail() {
+    return this.formulario.get('email');
+  }
+
+  get campoTelefono() {
+    return this.formulario.get('telefono');
+  }
+
+  get campoNumVitimas() {
+    return this.formulario.get('numVitimas');
+  }
+
+  // GETTERS PARA VER SE OS CAMPOS SON VÁLIDOS
+  get numParteValido(): boolean {
+    return this.campoNumParte.valid && this.campoNumParte.touched;
+  }
+
+  get matriculaValido(): boolean {
+    return this.campoMatricula.valid && this.campoMatricula.touched;
+  }
+
+  get gravidadeValido(): boolean {
+    return this.campoGravidade.valid && this.campoGravidade.touched;
+  }
+
+  get descricionValido(): boolean {
+    return this.campoDescricion.valid && this.campoDescricion.touched;
+  }
+
+  get dataValido(): boolean {
+    return this.campoData.valid && this.campoData.touched;
+  }
+
+  get emailValido(): boolean {
+    return this.campoEmail.valid && this.campoEmail.touched;
+  }
+
+  get telefonoValido(): boolean {
+    return this.campoTelefono.valid && this.campoTelefono.touched;
+  }
+
+  get numVitimasValido(): boolean {
+    return this.campoNumVitimas.valid && this.campoNumVitimas.touched;
+  }
+
+  // GETTERS PARA VER SE OS CAMPOS SON INVÁLIDOS
+  get numParteInvalido(): boolean {
+    return this.campoNumParte.invalid && this.campoNumParte.touched;
+  }
+
+  get matriculaInvalido(): boolean {
+    return this.campoMatricula.invalid && this.campoMatricula.touched;
+  }
+
+  get gravidadeInvalido(): boolean {
+    return this.campoGravidade.invalid && this.campoGravidade.touched;
+  }
+  
+  get descricionInvalido(): boolean {
+    return this.campoDescricion.invalid && this.campoDescricion.touched;
+  } 
+
+  get dataInvalido(): boolean {
+    return this.campoData.invalid && this.campoData.touched;
+  } 
+
+  get emailInvalido(): boolean {
+    return this.campoEmail.invalid && this.campoEmail.touched;
+  }
+
+  get telefonoInvalido(): boolean {
+    return this.campoTelefono.invalid && this.campoTelefono.touched;
+  }
+
+  get numVitimasInvalido(): boolean {
+    return this.campoNumVitimas.invalid && this.campoNumVitimas.touched;
+  }
 }
