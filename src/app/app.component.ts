ed08d0b10957eb419ff2a20d43ed8c3d19dee63c
JCSC79 <jcsandomingo@egmail.com>
2026-08-30 01:24:07 +0200

    Automático: Añadido T05E06-enlazado-bidireccional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = '02-TypeScript';
+  title: string = '17-enlazado-bidireccional';
 
-  // Declaramos unha propiedade de clase (variable) de tipo numérico inicializada a un valor
-  numero1: number = 5;
-  /* Declaramos unha propiedade de clase de tipo numérico non inicializada (engadimos nas opcións de compilador do tsconfig.json a seguinte clave
-    "strictPropertyInitialization": false,)
-  */
-  numero2: number;
+  resetearTitulo() {
+    this.title = '17-enlazado-bidireccional';
+  }
 }
