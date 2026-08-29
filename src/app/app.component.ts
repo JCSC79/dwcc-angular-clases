24a1e906bab676be45fbf36687c53718a3b39c4c
JCSC79 <jcsandomingo@egmail.com>
2026-08-30 01:24:33 +0200

    Automático: Añadido T05E20-ngTemplate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = '02-TypeScript';
+  title = 'ng-template';
 
-  // Declaramos unha propiedade de clase (variable) de tipo numérico inicializada a un valor
-  numero1: number = 5;
-  /* Declaramos unha propiedade de clase de tipo numérico non inicializada (engadimos nas opcións de compilador do tsconfig.json a seguinte clave
-    "strictPropertyInitialization": false,)
-  */
-  numero2: number;
+  // Exemplo 1: usando ng-template con ngIf
+  logueado: boolean = false;
+  usuaria: string = "Uxía";
+
+  // Exemplo 2: vistas personalizadas usando ng-template con *ngTemplateOutlet
+  produtos: any[] = [
+    { modelo: 'Carcasa 3 1/2 USB3', prezo: 19.95 },
+    { modelo: 'Carcasa 2 1/2 SATA', prezo: 5.95 },
+    { modelo: 'Carcasa 3 1/2 USB-C', prezo: 23.95 }
+  ];
 }
