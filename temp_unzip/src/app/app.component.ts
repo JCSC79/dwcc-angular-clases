24d968c08d8fef50da470f89579b4e4020d4b8a3
JCSC79 <jcsandomingo@egmail.com>
2026-08-30 01:44:08 +0200

    Automático: Añadido ejercicio de repaso Repaso-RepasoFormReactivos (solución)

--- temp_unzip/src/app/app.component.ts
+++ temp_unzip/src/app/app.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Factura } from './factura';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Exercicio de Repaso de Formularios Reactivos';
+
+  formularioFacturas: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.formularioFacturas = fb.group({
+      numero: ['', [Validators.required]],
+      dataEmision: ['', [Validators.required]],
+      razonSocial: ['', [Validators.required]],
+      CIF: ['', [Validators.required]]
+    });
+  }
+
+  crear(): void {
+    let factura: Factura = this.formularioFacturas.value;
+    console.table(factura);
+    this.formularioFacturas.reset();
+  }
+
+  cancelar(): void {
+    this.formularioFacturas.reset();
+  }
 }
