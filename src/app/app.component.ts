257d3beb6af6481ef760ea1298bc74ae2429f869
JCSC79 <jcsandomingo@egmail.com>
2026-08-30 01:24:48 +0200

    Automático: Añadido T05E28-HostListener-teclado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,57 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = '02-TypeScript';
+  title = 'HostListener para eventos de teclado e scroll';
+  contaEnter: number = 0; // Contador de pulsacións da tecla ENTER
+  contaShiftQ: number = 0; // Contador de pulsacións da combinación SHIFT + Q
+  contaClick: number = 0; // Contador de clics do rato
+  contaScroll: number = 0; // Contador de veces que se fai scroll
+  contaShiftDto: number = 0; // Contador de veces que se preme Shift dereito
+  contaClickDto: number = 0; // Contador de clics do botón dereito do rato
 
-  // Declaramos unha propiedade de clase (variable) de tipo numérico inicializada a un valor
-  numero1: number = 5;
-  /* Declaramos unha propiedade de clase de tipo numérico non inicializada (engadimos nas opcións de compilador do tsconfig.json a seguinte clave
-    "strictPropertyInitialization": false,)
-  */
-  numero2: number;
+  // Método que se executa cando se preme a tecla ENTER
+  @HostListener('window:keydown.enter') premerEnter() {
+    this.contaEnter++;
+  }
+
+  // Método que se executa cando se preme a combinación Shift + Q
+  @HostListener('window:keydown.shift.q') premerShiftQ() {
+    this.contaShiftQ++;
+  }
+
+  // Método que se executa cando se fai click co rato
+  @HostListener('window:click') facerClic() {
+    this.contaClick++;
+  }
+
+  // Método que se executa cando se fai scroll vertical na páxina
+  @HostListener('window:scroll') facerScroll() {
+    this.contaScroll++;
+  }
+
+  // Método que se executa cada vez que se preme unha tecla e que conta o número de veces que esa tecla é o Shift dereito
+  @HostListener('window:keydown', ['$event']) contarShiftDereito(evento: KeyboardEvent) {
+    if (evento.code === 'ShiftRight') {
+      this.contaShiftDto++;
+    }
+  }
+
+  // Método que se executa cada vez que se fai clic co rato e que conta o número de veces que se fai clic co botón dereito
+  @HostListener('window:mousedown', ['$event']) contarClicDereito(evento: MouseEvent) {
+    if (evento.button == 2) {
+      this.contaClickDto++;
+    }
+  }
+
+  // Este método evita que se abra o menú contextual ao facer clic co botón dereito
+  @HostListener('window:contextmenu', ['$event']) evitarMenuContextual(evento: MouseEvent) {
+    evento.preventDefault();
+  }
 }
