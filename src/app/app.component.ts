4ae56ca3632a4820996f438fa33587e491a8202a
JCSC79 <jcsandomingo@egmail.com>
2026-08-30 01:30:10 +0200

    Automático: Añadido T06E22-ficheiro-JSON

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = '02-TypeScript';
+  title = 'Ficheiro JSON';
 
-  // Declaramos unha propiedade de clase (variable) de tipo numérico inicializada a un valor
-  numero1: number = 5;
-  /* Declaramos unha propiedade de clase de tipo numérico non inicializada (engadimos nas opcións de compilador do tsconfig.json a seguinte clave
-    "strictPropertyInitialization": false,)
-  */
-  numero2: number;
+  // Array que recibirá os datos dende o ficheiro JSON
+  empregados: any[] = [];
+
+  // Engadimos unha propiedade de clase privada de tipo HttpClient
+  constructor(private clienteHTTP: HttpClient) {}
+
+  // Este método execútase cando a compoñente teña sido inicializada
+  ngOnInit(): void {
+    // Tomamos os datos do ficheiro JSON mediante unha solicitude HTTP e gardámolos no array 'empregados'
+    this.clienteHTTP
+      .get<any[]>('data/empregados.json')
+      .subscribe((datos) => {
+        this.empregados = datos;
+      })
+  }
 }
