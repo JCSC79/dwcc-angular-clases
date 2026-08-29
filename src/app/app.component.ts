806d9a74f5000ca0205b07640c9bc7fdec56be19
JCSC79 <jcsandomingo@egmail.com>
2026-08-30 01:29:42 +0200

    Automático: Añadido T06E07-servizos-observables-v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IncidenciasService } from './incidencias.service';
+import { FormularioAltaComponent } from './formulario-alta/formulario-alta.component';
+import { ListaxeIncidenciasComponent } from './listaxe-incidencias/listaxe-incidencias.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormularioAltaComponent,ListaxeIncidenciasComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [IncidenciasService]
 })
 export class AppComponent {
-  title = '02-TypeScript';
-
-  // Declaramos unha propiedade de clase (variable) de tipo numérico inicializada a un valor
-  numero1: number = 5;
-  /* Declaramos unha propiedade de clase de tipo numérico non inicializada (engadimos nas opcións de compilador do tsconfig.json a seguinte clave
-    "strictPropertyInitialization": false,)
-  */
-  numero2: number;
+  title = 'servizos-observables-v2';
 }
