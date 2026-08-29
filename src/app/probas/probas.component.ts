aa47cf76cdea34ff9d44d72af234362b0d4e554a
JCSC79 <jcsandomingo@egmail.com>
2026-08-30 01:29:52 +0200

    Automático: Añadido T06E12-rutas-aninhadas

---
+++ src/app/probas/probas.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-probas',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './probas.component.html',
+  styleUrl: './probas.component.css'
+})
+export class ProbasComponent {
+
+}
