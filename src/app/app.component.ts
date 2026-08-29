2efd58f97a0b3227ed1adcfebb4f5c31479de43b
JCSC79 <jcsandomingo@egmail.com>
2026-08-30 01:23:57 +0200

    Automático: Añadido T05E02-OlaMundo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'ola-mundo',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = '02-TypeScript';
-
-  // Declaramos unha propiedade de clase (variable) de tipo numérico inicializada a un valor
-  numero1: number = 5;
-  /* Declaramos unha propiedade de clase de tipo numérico non inicializada (engadimos nas opcións de compilador do tsconfig.json a seguinte clave
-    "strictPropertyInitialization": false,)
-  */
-  numero2: number;
+  title = '01-OlaMundo';
 }
