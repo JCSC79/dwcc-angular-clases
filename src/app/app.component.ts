69db8fc8f00f9311e233945e56f57bc8ba314350
JCSC79 <jcsandomingo@egmail.com>
2026-08-30 01:29:57 +0200

    Automático: Añadido T06E15-rutas-guards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = '02-TypeScript';
-
-  // Declaramos unha propiedade de clase (variable) de tipo numérico inicializada a un valor
-  numero1: number = 5;
-  /* Declaramos unha propiedade de clase de tipo numérico non inicializada (engadimos nas opcións de compilador do tsconfig.json a seguinte clave
-    "strictPropertyInitialization": false,)
-  */
-  numero2: number;
+  title = 'rutas-guards';
 }
