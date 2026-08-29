21d20318eb1a983f25ac5df7de6719986125f85a
JCSC79 <jcsandomingo@egmail.com>
2026-08-30 01:29:59 +0200

    Automático: Añadido T06E16-formularios-reactivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,41 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = '02-TypeScript';
+  title = 'formularios-reactivos';
+  formularioAccidente: FormGroup; // Definimos o FormGroup que irá asociado ao noso formulario HTML
 
-  // Declaramos unha propiedade de clase (variable) de tipo numérico inicializada a un valor
-  numero1: number = 5;
-  /* Declaramos unha propiedade de clase de tipo numérico non inicializada (engadimos nas opcións de compilador do tsconfig.json a seguinte clave
-    "strictPropertyInitialization": false,)
-  */
-  numero2: number;
+  // Declaramos unha propiedade de tipo FormBuilder (que nos permitirá crear de xeito simple o formulario reactivo)
+  constructor(private elaborador: FormBuilder) {
+    this.formularioAccidente = this.elaborador.group({
+      numParte: new FormControl(),
+      matricula: new FormControl(),
+      gravidade: new FormControl(),
+      descricion: new FormControl(),
+      data: new FormControl(),
+      email: new FormControl(),
+      telefono: new FormControl(),
+      numVitimas: new FormControl()
+    });
+
+    // Definimos o comportamento do noso formulario cada vez que sufra un cambio
+    this.formularioAccidente.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe(valor => {
+        console.table(valor);
+      });
+  }
+
+  rexistrar(evento: Event) {
+    evento.preventDefault(); // Evitamos o comportamento por defecto do navegador que borraría o formulario ao lanzar un "submit"
+    console.table(this.formularioAccidente.value);
+  }
 }
