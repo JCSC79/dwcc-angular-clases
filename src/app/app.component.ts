b8ce9c37f22ad1152ec0d0c8599d98d7670ed942
JCSC79 <jcsandomingo@egmail.com>
2026-08-30 08:32:47 +0200

    docs(T06E02): Mejorar comentarios y crear README

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,12 @@ import { Grupo } from './grupo';
 import { Usuario } from './usuario';
 import { CommonModule } from '@angular/common';
 
+/**
+ * Componente que demuestra Clases en TypeScript
+ * 
+ * Crear moldes para objetos con propiedades y métodos
+ */
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -12,10 +18,12 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  // title: propiedade que almacena un valor
   title = 'clases';
 
   // Inicializamos dous obxectos da clase "Grupo" usando o seu construtor
   administracion = new Grupo(0, 'Administración');
+  // desenvolvemento: propiedade que almacena un valor
   desenvolvemento = new Grupo(1, 'Desenvolvemento');
 
   // Inicializamos un obxecto da clase "Usuario" usando o seu construtor
