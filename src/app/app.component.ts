96f5f4a3568fb5490043b2a24fbe6140dc42c35e
JCSC79 <jcsandomingo@egmail.com>
2026-08-30 08:32:22 +0200

    docs(T05E26): Mejorar comentarios y crear README

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { LigadoDirective } from './directives/ligado.directive';
 
+/**
+ * Componente que demuestra Componente de Angular
+ * 
+ * Demuestra conceptos de Angular
+ */
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -9,5 +15,6 @@ import { LigadoDirective } from './directives/ligado.directive';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  // title: propiedade que almacena un valor
   title = 'HostBinding';
 }
