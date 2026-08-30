0d42f7ae05a70463adcaaa8f9fedde66bae82162
JCSC79 <jcsandomingo@egmail.com>
2026-08-30 08:31:45 +0200

    docs(T05E20): Mejorar comentarios y crear README

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
+/**
+ * Componente que demuestra Componente de Angular
+ * 
+ * Demuestra conceptos de Angular
+ */
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -9,10 +15,12 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  // title: propiedade que almacena un valor
   title = 'ng-template';
 
   // Exemplo 1: usando ng-template con ngIf
   logueado: boolean = false;
+  // usuaria: propiedade que almacena un valor
   usuaria: string = "Uxía";
 
   // Exemplo 2: vistas personalizadas usando ng-template con *ngTemplateOutlet
