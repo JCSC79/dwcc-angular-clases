d382973f207c441cf6d2e29f07b6a2ac2963c8f6
JCSC79 <jcsandomingo@egmail.com>
2026-08-30 08:31:39 +0200

    docs(T05E19): Mejorar comentarios y crear README

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+/**
+ * Componente que demuestra Componente de Angular
+ * 
+ * Demuestra conceptos de Angular
+ */
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,6 +16,7 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  // title: propiedade que almacena un valor
   title = 'ngPlural';
   contador = 0;
   incrementar() { this.contador++; }
